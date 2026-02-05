ab73b2c1f2e10da5dbfac775f4b488b346d135d6
ngthanhvu <nguyenthanhvudvfb@gmail.com>
2026-02-05 16:49:59 +0700

    Refactor: add cleanup step for old docker-compose override file in build script

--- run-build.sh
+++ run-build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
+# Cleanup old override file from previous deployment strategy
+rm -f docker-compose.override.yml
+
 docker compose -f docker-compose.prod.yml up -d --build
